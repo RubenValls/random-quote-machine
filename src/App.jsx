7b934a117b1c8b440470395d2073f46c89991e33
Rubén Valls <r.vallsaparici@gmail.com>
2023-07-25 18:49:45 +0200

    added changing of colors

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,32 @@
 import { useState, useEffect } from 'react';
 import { quotes } from './assets/quotes.js'
+import { colors } from './assets/colors.js'
 import './App.css'
 import { BiSolidQuoteLeft } from 'react-icons/bi';
 
 function App() {
   const allQuotes = quotes
+  const allColors = colors
   const [selectedQuote, setSelectedQuote] = useState({});
+  const [selectedColor, setSelectedColor] = useState('');
 
   useEffect(() => {
     setSelectedQuote(allQuotes[Math.floor(Math.random() * allQuotes.length - 1)])
-  }, [allQuotes]);
+    setSelectedColor(allColors[Math.floor(Math.random() * allColors.length - 1)])
+  }, [allQuotes, allColors]);
 
   const newQuote = () => {
     setSelectedQuote(allQuotes[Math.floor(Math.random() * allQuotes.length - 1)])
+    setSelectedColor(allColors[Math.floor(Math.random() * allColors.length - 1)])
   }
 
   return (
-    <div id='container'>
+    <div id='container' style={{backgroundColor: selectedColor}}>
       <div id='quote-box'>
-        <div id='text'>
+        <div id='text' style={{color: selectedColor}}>
           <h1><BiSolidQuoteLeft id='quote-icon'/>{selectedQuote?.quote}</h1>
         </div>
-        <div id='author'>
+        <div id='author' style={{color: selectedColor}}>
           <p>- {selectedQuote?.author}</p>
         </div>
         <div id='buttons'>
---
+++ src/assets/colors.js
@@ -0,0 +1,3 @@
+export const colors= ['aqua', 'black', 'blue', 'fuchsia', 'gray', 'green', 
+'lime', 'maroon', 'navy', 'olive', 'orange', 'purple', 'red', 
+'silver', 'teal', 'white', 'yellow'];
